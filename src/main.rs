d0eb941e01f85e73bc1830e46dd45c179667cccf
Salvatore Vitale <salvatore.vitale@ing.com>
2021-09-18 21:10:47 +0200

    add cors headers

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,36 @@ mod conjugation;
 mod dictionary;
 use conjugation::retrieve_conjugation;
 use dictionary::retrieve_translation;
+// use rocket::http::{ContentType, Header, Method};
+use rocket::http::{Header};
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
 
 #[macro_use]
 extern crate rocket;
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        println!("Setting access control allow origin");
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        // response.set_header(Header::new(
+        //     "Access-Control-Allow-Methods",
+        //     "POST, GET, PATCH, OPTIONS",
+        // ));
+        // response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        // response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[get("/conjugate/<verb>")]
 async fn conjugate(verb: &str) -> String {
@@ -18,5 +45,5 @@ async fn translate(word: &str) -> String {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![conjugate, translate])
+    rocket::build().attach(CORS).mount("/", routes![conjugate, translate])
 }
